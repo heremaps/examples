597fbcf8b2f678e39dcb3cc61b5c4aa3c85f4f55
Jason Fox <jason.fox@nokia.com>
2013-09-10 14:32:09 +0200

    Add text icon on marker example.

---
+++ maps_js/libs/text-on-icon-marker.js
@@ -0,0 +1,101 @@
+﻿
+function extend(B, A) {
+	function I() {}
+	I.prototype = A.prototype;
+	B.prototype = new I();
+	B.prototype.constructor = B;
+}
+
+function TextOnIconMarker (coord, props)  {
+	nokia.maps.map.Marker.call(this, coord, props);
+	var that = this;
+	that.body = document.body || document._documentElement;
+	
+	that.createDefacedIcon = function(image){
+		
+		var op = nokia.maps.gfx.IDL.opcodes,
+			idlImage = new nokia.maps.gfx.IDL(
+				[op.BEGIN_2D_IMAGE, image.width, image.height, "",
+				op.MOVE_TO, 0, 0,
+				op.DRAW_IMAGE, that._imgElement, 
+					0, 0, image.width, image.height, 0, 0, 
+					image.width, image.height,
+				op.DRAW_IMAGE, that._textAsImgElement, 
+					0, 0, image.width, image.height, 0, 0, 
+					image.width,image.height
+				
+				]); 
+		that.defacedIcon = 
+		new nokia.maps.gfx.GraphicsImage(idlImage);
+		
+	}
+	that.addTextToDOM = function(image){
+		var GraphicsImage = nokia.maps.gfx.GraphicsImage,
+		Color = nokia.maps.gfx.Color,
+		parseCss = Color.parseCss;
+
+		// Create a new graphics image.
+		var textImage = new GraphicsImage(function (graphics, graphicsImage) {
+			// This will clear the canvas and set it's size.
+			graphics.beginImage(image.width, image.height, "");
+			graphics.set("font", "bold 12px verdana");	
+			//center the text relatively to anchor point;
+			graphics.set("textAlign", "center");
+			graphics.set("fillColor", parseCss("black",1.0));
+			graphics.set("strokeColor", parseCss("white",1.0));
+				
+			//center the text to be in the center of the canvas
+			graphics.strokeText(that.get("text"), image.width/2, image.height/2);
+			graphics.fillText(that.get("text"), image.width/2, image.height/2);
+		});
+		
+		that._textAsImgElement = textImage.createElement();
+		that._textAsImgElement.style.display = "none";
+		that.body.appendChild(that._textAsImgElement, that.createDefacedIcon(image) );
+	}
+	that.addImageToDOM = function (image) {
+			that._imgElement = image.createElement();
+			that._imgElement.style.display = "none";				
+			that.body.appendChild(that._imgElement, that.addTextToDOM( image));
+	}
+
+	that.updateIcon = function (){
+		if(that._textAsImgElement){
+			that._textAsImgElement.parentNode.removeChild(that._textAsImgElement);
+		}
+		if(that._imgElement){
+			that._imgElement.parentNode.removeChild(that._imgElement);
+		}		
+		var baseBitmap = new nokia.maps.gfx.BitmapImage(that.get("icon").src);
+		baseBitmap.prepare(that.addImageToDOM, null);
+	}
+	
+	that.updateIcon();
+	that.addObserver("text", that.updateIcon);
+	that.addObserver("icon", that.updateIcon);
+	
+}
+
+extend(TextOnIconMarker,
+		nokia.maps.map.Marker);
+
+
+TextOnIconMarker.prototype.init = function (props) {
+	that = this;
+	if (props){
+		that.set("text", props.text !== undefined ? props.text : "");
+	} else {
+		that.set("text", "");
+	}
+}
+
+TextOnIconMarker.prototype.getIconForRendering  = function (doc) {
+	img1 = nokia.maps.map.Marker.prototype.getIconForRendering.call(this, doc);	
+	if (document.all && !document.addEventListener) {
+		// Return basic marker for older browsers.
+    	return img1;
+	}
+	return (this.defacedIcon) ? this.defacedIcon : img1 ;
+
+}
+ 
